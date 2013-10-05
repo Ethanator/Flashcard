34e6c82b26167d3160e009bcbaa8dfc7f6280f47
Student <ychen18@nd.edu>
2013-10-05 00:33:29 -0400

    Added Deck Collection View Cell

---
+++ Flashcard/FCDeckCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FCDeckCollectionViewCell.h
+//  Flashcard
+//
+//  Created by Student on 10/5/13.
+//  Copyright (c) 2013 Sean T Fitzgerald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCDeckCollectionViewCell : UICollectionViewCell
+
+@end
